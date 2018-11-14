71bbcfbe96c9398b438fb264d0087751a79ecf80
AntonKechashin <atk@sitecore.net>
2018-11-14 14:53:00 +0200

    Fix for SXA Storefront 1.2.205

---
+++ src/Sitecore.Support.292946/Commerce/XA/Feature/Cart/Models/MinicartRenderingModel.cs
@@ -0,0 +1,43 @@
+﻿namespace Sitecore.Support.Commerce.XA.Feature.Cart.Models
+{
+  using Sitecore.Commerce.XA.Foundation.Common.Context;
+  using Sitecore.Mvc.Extensions;
+  using Sitecore.Mvc.Presentation;
+
+  public class MinicartRenderingModel : Sitecore.Commerce.XA.Feature.Cart.Models.MinicartRenderingModel
+  {
+    public MinicartRenderingModel([NotNull] IStorefrontContext storefrontContext) : base(storefrontContext)
+    {
+      this.StorefrontContext = storefrontContext;
+    }
+
+    public new string CheckoutLink { get; set; }
+
+    public new bool HideCheckoutButton { get; set; }
+
+    public new IStorefrontContext StorefrontContext { get; set; }
+
+    public new string TopLink { get; set; }
+
+    public new string ViewCartLink { get; set; }
+
+    public new void Initialize()
+    {
+      var currentRendering = RenderingContext.CurrentOrNull.ValueOrDefault(context => context.Rendering);
+      if (currentRendering != null)
+      {
+        var checkoutPath = currentRendering.Parameters["Checkout Link"] ?? string.Empty;
+        #region Changed code
+        var viewCartPath = currentRendering.Parameters["View Cart Link"] ?? string.Empty; // removed this.TopLink initialization
+        #endregion
+        var checkoutPageItem = Context.Database.GetItem(checkoutPath);
+        var viewCartPageItem = Context.Database.GetItem(viewCartPath);
+        this.CheckoutLink = this.StorefrontContext.StorefrontUri(checkoutPageItem).Path;
+        #region Changed code
+        this.ViewCartLink = this.TopLink = this.StorefrontContext.StorefrontUri(viewCartPageItem).Path; // added this.TopLink initialization
+        #endregion
+        this.HideCheckoutButton = MainUtil.GetBool(currentRendering.Parameters["Hide Checkout Button"], false);
+      }
+    }
+  }
+}
---
+++ src/Sitecore.Support.292946/Commerce/XA/Feature/Cart/Repositories/MinicartRepository.cs
@@ -0,0 +1,23 @@
+﻿namespace Sitecore.Support.Commerce.XA.Feature.Cart.Repositories
+{
+  using Sitecore.Commerce.XA.Feature.Cart.Models;
+  using Sitecore.Commerce.XA.Foundation.Common.Context;
+  using Sitecore.Commerce.XA.Foundation.Common.Models;
+  using Sitecore.Commerce.XA.Foundation.Connect;
+  using Sitecore.Commerce.XA.Foundation.Connect.Managers;
+
+  public class MinicartRepository : Sitecore.Commerce.XA.Feature.Cart.Repositories.MinicartRepository
+  {
+    public MinicartRepository(IModelProvider modelProvider, ICartManager cartManager, ISiteContext siteContext) : base(modelProvider, cartManager, siteContext)
+    {
+    }
+
+    public override MinicartRenderingModel GetMinicartModel(IStorefrontContext storefrontContext, IVisitorContext visitorContext)
+    {
+      Sitecore.Support.Commerce.XA.Feature.Cart.Models.MinicartRenderingModel model = base.ModelProvider.GetModel<Sitecore.Support.Commerce.XA.Feature.Cart.Models.MinicartRenderingModel>();
+      base.Init(model);
+      model.Initialize();
+      return model;
+    }
+  }
+}
